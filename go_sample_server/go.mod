5dd7ed5678ba66a92e9f7e5dc02e0bda9892db58
szstonelee <szstonelee@Tongs-MacBook-Pro.local>
2020-06-22 19:47:47 -0700

    grpc support

---
+++ go_sample_server/go.mod
@@ -0,0 +1,8 @@
+module github.com/szstonelee/redisoo/goserver
+
+go 1.14
+
+require (
+	github.com/golang/protobuf v1.3.3
+	google.golang.org/grpc v1.29.1
+)
---
+++ go_sample_server/server.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+
+	pb "github.com/szstonelee/redisoo/goserver/redisoo"
+
+	"google.golang.org/grpc"
+)
+
+const (
+	port = ":40051"
+)
+
+func reverse(in []byte) []byte {
+	for i, j := 0, len(in)-1; i < j; i, j = i+1, j-1 {
+		in[i], in[j] = in[j], in[i]
+	}
+	return in
+}
+
+type server struct {
+	pb.UnimplementedRedisooServer
+}
+
+func (s *server) GetString(ctx context.Context, in *pb.GetStringRequest) (*pb.GetStringResponse, error) {
+	log.Printf("Received: %v", in.GetKey())
+	var r pb.GetStringResponse
+	r.Status = pb.GetStringResponse_STATUS_REPLACE
+	r.Value = reverse(in.Key)
+	log.Printf("Reply status: %v, value: %v\n", r.Status, r.Value)
+	return &r, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterRedisooServer(s, &server{})
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
